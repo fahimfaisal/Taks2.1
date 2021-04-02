19b41c64353e48055a2400732431f40945beb03b
fahimfaisal <navid2010ps@gmail.com>
2021-04-02 17:40:54 +1100

    Final touch

--- app/src/main/java/com/example/unitconverter/MainActivity.java
+++ app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -1,30 +1,20 @@
 package com.example.unitconverter;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.LinearLayoutCompat;
-import androidx.core.widget.TextViewCompat;
-
-import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.digidemic.unitof.UnitOf;
-
-import org.w3c.dom.Text;
-
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.security.PublicKey;
+
 
 public class MainActivity extends AppCompatActivity implements  AdapterView.OnItemSelectedListener {
 
@@ -32,7 +22,8 @@ public class MainActivity extends AppCompatActivity implements  AdapterView.OnIt
     TextView view1, view2, view3;
     EditText edit;
     Spinner spinner;
-    private LinearLayout container;
+    Context error;
+    Context novalue;
 
 
     @Override
@@ -48,8 +39,8 @@ public class MainActivity extends AppCompatActivity implements  AdapterView.OnIt
         view2 = (TextView) findViewById(R.id.textView2);
         view3 = (TextView) findViewById(R.id.textView3);
         edit = (EditText) findViewById(R.id.input);
-        container= (LinearLayout) findViewById(R.id.textview) ;
-
+        error  = getApplicationContext();
+        novalue = getApplicationContext();
         spinner = (Spinner) findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
         R.array.metrics, android.R.layout.simple_spinner_item);
@@ -59,7 +50,143 @@ public class MainActivity extends AppCompatActivity implements  AdapterView.OnIt
         spinner.setOnItemSelectedListener(this);
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
+        emptyTextView();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    private double round(double value, int places)
+    {
+
+        BigDecimal bd;
+        bd = BigDecimal.valueOf(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
+    }
+
+
+    public void ConvertTemp(View view)
+    {
+
+        String text = spinner.getSelectedItem().toString();
+
+        if(edit.getText().toString().isEmpty())
+        {
+            CharSequence empty = "Please Enter a Value";
+            Toast.makeText(novalue, empty, Toast.LENGTH_SHORT).show();
+        }
+
+        else
+        {
+
+
+            if(text.equals(spinner.getItemAtPosition(1).toString()))
+            {
+
+                Double input = Double.parseDouble(edit.getText().toString());
+
+                Double ctof = round(new UnitOf.Temperature().fromCelsius(input).toFahrenheit(),2);
+                Double ctok = round( new UnitOf.Temperature().fromCelsius(input).toKelvin(),2);
+
+
+                view1.setText(ctof.toString() + " Fahrenheit");
+                view2.setText(ctok.toString() + " Kelvin");
+                view3.setText("");
+            }
+            else
+            {
+                showError();
+            }
+
+        }
+
+    }
+
+
+
+    public void ConvertWeight(View view)
+    {
+
+
+        String text = spinner.getSelectedItem().toString();
+
+        if(edit.getText().toString().isEmpty())
+        {
+            CharSequence empty = "Please Enter a Value";
+            Toast.makeText(novalue, empty, Toast.LENGTH_SHORT).show();
+        }
+
+        else
+        {
+
+
+            if(text.equals(spinner.getItemAtPosition(2).toString()))
+            {
+
+                Double input = Double.parseDouble(edit.getText().toString());
+
+                Double ktog = round(new UnitOf.Mass().fromKilograms(input).toGrams(),2);
+                Double ktoo = round(new UnitOf.Mass().fromKilograms(input).toOuncesUS(),2);
+                Double ktop = round(new UnitOf.Mass().fromKilograms(input).toPounds(),2);
+
+                view1.setText(ktog.toString() + " Gram");
+                view2.setText(ktoo.toString() + " Ounce(Oz)");
+                view3.setText(ktop.toString() + " Pound(lb)");
+            }
+            else
+            {
+                showError();
+            }
+
+        }
+
+    }
+
+    public void ConvertDistance(View view)
+    {
+
+        String text = spinner.getSelectedItem().toString();
+
+        if(edit.getText().toString().isEmpty())
+        {
+            CharSequence empty = "Please Enter a Value";
+            Toast.makeText(novalue, empty, Toast.LENGTH_SHORT).show();
+        }
+
+        else
+        {
+
+
+            if(text.equals(spinner.getItemAtPosition(0).toString()))
+            {
+
+                Double input = Double.parseDouble(edit.getText().toString());
+
+                Double mtocm = round(new UnitOf.Length().fromMeters(input).toCentimeters(),2);
+                Double mtofeet =round(new UnitOf.Length().fromMeters(input).toFeet(),2);
+                Double mtoi = round(new UnitOf.Length().fromMeters(input).toInches(), 2);
+
+
+                view1.setText(mtocm.toString() + " Centimetre");
+                view2.setText(mtofeet.toString() + " Feet");
+                view3.setText(mtoi.toString() + " Inch");
+            }
+            else
+            {
+               showError();
+            }
+
+         }
+
+
+    }
 
 
     public void emptyTextView()
@@ -73,114 +200,11 @@ public class MainActivity extends AppCompatActivity implements  AdapterView.OnIt
     {
         emptyTextView();
 
+        CharSequence showmsg = "Please Select The Correct Conversion Icon";
 
-        Context context = getApplicationContext();
-        CharSequence text = "Please Select The Correct Conversion Icon";
-
-        Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
-
-
-
-
+        Toast.makeText(error, showmsg, Toast.LENGTH_SHORT).show();
     }
 
-
-
-    public void ConvertTemp(View view)
-    {
-        Double input = Double.parseDouble(edit.getText().toString());
-        Double ctof = round(new UnitOf.Temperature().fromCelsius(input).toFahrenheit(),2);
-        Double ctok = round( new UnitOf.Temperature().fromCelsius(input).toKelvin(),2);
-
-
-
-        String text = spinner.getSelectedItem().toString();
-
-        if (text.equals(spinner.getItemAtPosition(1).toString()))
-        {
-
-
-            view1.setText(ctof.toString() + " Fahrenheit");
-            view2.setText(ctok.toString() + " Kelvin");
-            view3.setText("");
-
-
-
-        }
-
-        else
-        {
-
-            showError();
-        }
-
-}
-
-
-
-    public void ConvertWeight(View view) {
-
-        Double input = Double.parseDouble(edit.getText().toString());
-        Double ktog = round(new UnitOf.Mass().fromKilograms(input).toGrams(),2);
-        Double ktoo = round(new UnitOf.Mass().fromKilograms(input).toOuncesUS(),2);
-        Double ktop = round(new UnitOf.Mass().fromKilograms(input).toPounds(),2);
-
-        String text = spinner.getSelectedItem().toString();
-
-        if ( text.equals(spinner.getItemAtPosition(2).toString()))
-        {
-
-            view1.setText(ktog.toString() + " Grams");
-            view2.setText(ktoo.toString() + " Ounces");
-            view3.setText(ktop.toString() + " Pounds");
-        }
-
-        else
-        {
-            showError();
-
-        }
-
-    }
-
-    public void ConvertDistance(View view) {
-        Double input = Double.parseDouble(edit.getText().toString());
-
-        Double mtocm = round(new UnitOf.Length().fromMeters(input).toCentimeters(),2);
-        Double mtofeet =round(new UnitOf.Length().fromMeters(input).toFeet(),2);
-        Double mtoi = round(new UnitOf.Length().fromMeters(input).toInches(), 2);
-
-        String text = spinner.getSelectedItem().toString();
-
-        if (text.equals(spinner.getItemAtPosition(0).toString())) {
-
-            view1.setText(mtocm.toString() + " Centimeters");
-            view2.setText(mtofeet.toString()+ " Feet");
-            view3.setText(mtoi.toString()+ " Inches");
-        } else {
-            showError();
-        }
-
-
-    }
-
-    private static double round(double value, int places) {
-
-        BigDecimal bd;
-        bd = BigDecimal.valueOf(value);
-        bd = bd.setScale(places, RoundingMode.HALF_UP);
-        return bd.doubleValue();
-    }
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        emptyTextView();
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
 }
 
 
