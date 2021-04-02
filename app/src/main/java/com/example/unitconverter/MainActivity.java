a19d77836b17170b68538c04061d80b5e9fb16db
fahimfaisal <navid2010ps@gmail.com>
2021-04-02 16:15:05 +1100

    Correcting fonts

--- app/src/main/java/com/example/unitconverter/MainActivity.java
+++ app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.unitconverter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.LinearLayoutCompat;
+import androidx.core.widget.TextViewCompat;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -16,6 +20,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 import com.digidemic.unitof.UnitOf;
 
+import org.w3c.dom.Text;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.security.PublicKey;
@@ -27,6 +33,7 @@ public class MainActivity extends AppCompatActivity implements  AdapterView.OnIt
     EditText edit;
     Spinner spinner;
     private LinearLayout container;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -52,18 +59,9 @@ public class MainActivity extends AppCompatActivity implements  AdapterView.OnIt
         spinner.setOnItemSelectedListener(this);
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String text = parent.getItemAtPosition(position).toString();
-        emptyTextView();
-        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
 
-    }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
 
-    }
     public void emptyTextView()
     {
         view1.setText("");
@@ -71,14 +69,28 @@ public class MainActivity extends AppCompatActivity implements  AdapterView.OnIt
         view3.setText("");
     }
 
+    public void showError()
+    {
+        emptyTextView();
+
+
+        Context context = getApplicationContext();
+        CharSequence text = "Please Select The Correct Conversion Icon";
+
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+
+
+
+
+    }
+
 
 
     public void ConvertTemp(View view)
     {
         Double input = Double.parseDouble(edit.getText().toString());
-//        Double input = Double.parseDouble(val);
-        Double ctof = new UnitOf.Temperature().fromCelsius(input).toFahrenheit();
-        Double ctok = new UnitOf.Temperature().fromCelsius(input).toKelvin();
+        Double ctof = round(new UnitOf.Temperature().fromCelsius(input).toFahrenheit(),2);
+        Double ctok = round( new UnitOf.Temperature().fromCelsius(input).toKelvin(),2);
 
 
 
@@ -88,8 +100,8 @@ public class MainActivity extends AppCompatActivity implements  AdapterView.OnIt
         {
 
 
-            view1.setText(ctof.toString());
-            view2.setText(ctok.toString());
+            view1.setText(ctof.toString() + " Fahrenheit");
+            view2.setText(ctok.toString() + " Kelvin");
             view3.setText("");
 
 
@@ -98,8 +110,8 @@ public class MainActivity extends AppCompatActivity implements  AdapterView.OnIt
 
         else
         {
-            emptyTextView();
-            view1.setText("error");
+
+            showError();
         }
 
 }
@@ -109,23 +121,23 @@ public class MainActivity extends AppCompatActivity implements  AdapterView.OnIt
     public void ConvertWeight(View view) {
 
         Double input = Double.parseDouble(edit.getText().toString());
-
-        Double ktog = new UnitOf.Mass().fromKilograms(input).toGrams();
-        Double ktoo = new UnitOf.Mass().fromKilograms(input).toOuncesUS();
-        Double ktop = new UnitOf.Mass().fromKilograms(input).toPounds();
+        Double ktog = round(new UnitOf.Mass().fromKilograms(input).toGrams(),2);
+        Double ktoo = round(new UnitOf.Mass().fromKilograms(input).toOuncesUS(),2);
+        Double ktop = round(new UnitOf.Mass().fromKilograms(input).toPounds(),2);
 
         String text = spinner.getSelectedItem().toString();
 
         if ( text.equals(spinner.getItemAtPosition(2).toString()))
         {
 
-            view1.setText(ktog.toString());
-            view2.setText(ktoo.toString());
-            view3.setText(ktop.toString());
+            view1.setText(ktog.toString() + " Grams");
+            view2.setText(ktoo.toString() + " Ounces");
+            view3.setText(ktop.toString() + " Pounds");
         }
 
-        else {
-            view1.setText("error");
+        else
+        {
+            showError();
 
         }
 
@@ -134,30 +146,40 @@ public class MainActivity extends AppCompatActivity implements  AdapterView.OnIt
     public void ConvertDistance(View view) {
         Double input = Double.parseDouble(edit.getText().toString());
 
-        Double mtocm = new UnitOf.Length().fromMeters(input).toCentimeters();
-        Double mtofeet =new UnitOf.Length().fromMeters(input).toFeet();
-        Double ktoi =  new UnitOf.Length().fromMeters(input).toInches();
+        Double mtocm = round(new UnitOf.Length().fromMeters(input).toCentimeters(),2);
+        Double mtofeet =round(new UnitOf.Length().fromMeters(input).toFeet(),2);
+        Double mtoi = round(new UnitOf.Length().fromMeters(input).toInches(), 2);
 
         String text = spinner.getSelectedItem().toString();
 
         if (text.equals(spinner.getItemAtPosition(0).toString())) {
 
-            view1.setText(mtofeet.toString());
-            Double ola= round(mtofeet,2);
-            view2.setText(ola.toString());
-            view3.setText(ktoi.toString());
+            view1.setText(mtocm.toString() + " Centimeters");
+            view2.setText(mtofeet.toString()+ " Feet");
+            view3.setText(mtoi.toString()+ " Inches");
         } else {
-            view1.setText("error");
+            showError();
         }
+
 
     }
 
     private static double round(double value, int places) {
-        if (places < 0) throw new IllegalArgumentException();
+
         BigDecimal bd;
         bd = BigDecimal.valueOf(value);
         bd = bd.setScale(places, RoundingMode.HALF_UP);
         return bd.doubleValue();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        emptyTextView();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
 
